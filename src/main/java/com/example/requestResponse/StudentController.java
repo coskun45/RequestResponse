a95183c7fba15832b2c75574a55094f21e2b6dcd
ecoskun <eyuep.coskun@adesso.de>
2023-08-31 12:10:20 +0200

    StudentController class erstellt, wo httpmethods implementiert werden soll

---
+++ src/main/java/com/example/requestResponse/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.requestResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    // http methods hier zu implementieren
+
+    //get
+
+
+
+
+
+    //post
+
+
+
+
+
+
+
+    //Put
+
+
+
+
+
+
+    //delete
+}
